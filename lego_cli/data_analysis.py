164352c3a77c9b4cf5ee5f7e16bcacae9d5564d8
Aurelien R <orel.reynaud@gmail.com>
2024-02-05 23:15:13 +0100

    flake8 format

--- lego_cli/data_analysis.py
+++ lego_cli/data_analysis.py
@@ -3,19 +3,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+
 def analyze_by_theme(csv_path, theme_filter=None, subtheme_filter=None):
     """ Analyzes LEGO data by theme and subtheme. """
     lego_data = pd.read_csv(csv_path)
-    
+
     # Grouping by Year, Theme, and Subtheme
-    theme_counts_by_year = lego_data.groupby(['Year', 'Theme', 'Subtheme']).size().reset_index(name='Count')
-    
+    theme_counts_by_year = lego_data.groupby(
+        ['Year', 'Theme', 'Subtheme']).size().reset_index(name='Count')
+
     # Filtering by Theme and Subtheme if specified
     if theme_filter:
-        theme_counts_by_year = theme_counts_by_year[theme_counts_by_year['Theme'] == theme_filter]
+        theme_counts_by_year = theme_counts_by_year[
+            theme_counts_by_year['Theme'] == theme_filter]
     if subtheme_filter:
-        theme_counts_by_year = theme_counts_by_year[theme_counts_by_year['Subtheme'] == subtheme_filter]
-    
+        theme_counts_by_year = theme_counts_by_year[
+            theme_counts_by_year['Subtheme'] == subtheme_filter]
+
     for (theme, subtheme), df in theme_counts_by_year.groupby(['Theme', 'Subtheme']):
         if df['Year'].nunique() > 1:  # Plot only if there's data for more than one year
             plt.figure(figsize=(10, 6))
@@ -27,4 +31,3 @@ def analyze_by_theme(csv_path, theme_filter=None, subtheme_filter=None):
             plt.grid(True)
             plt.tight_layout()  # Adjust layout to ensure everything fits without overlap
             plt.show()
-
