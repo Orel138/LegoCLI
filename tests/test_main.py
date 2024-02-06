855714233c0ced0e803b07c0ac7e6c23d706aaae
Orel138 <orel.reynaud@gmail.com>
2024-02-06 22:23:47 +0100

    👨‍🔬 Create 1st test with pytest.

---
+++ tests/test_main.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+from pathlib import Path
+import pytest
+from unittest.mock import patch
+
+sys.path.append(str(Path(__file__).parent.parent / 'lego_cli'))
+
+from lego_cli.main import LegoCLI
+
+# Test to verify the analyze_lego_data method with a mock
+@patch('matplotlib.pyplot.show')
+@patch('lego_cli.main.analyze_by_theme')
+def test_analyze_lego_data(mock_analyze, mock_show):
+    cli = LegoCLI()
+    cli.option.analyze_by_theme = True
+    cli.option.csv_path = 'tests/Brickset-MySets-all.csv'
+    cli.option.theme = 'Star Wars'
+    cli.option.subtheme = 'Episode IV'
+    cli.analyze_lego_data()
+    mock_analyze.assert_called_once_with(
+        'tests/Brickset-MySets-all.csv',
+        'Star Wars',
+        'Episode IV'
+        )
