03741fa706f98b707dfbd22f08326e4ea89584e6
Aurelien R <orel.reynaud@gmail.com>
2024-02-08 00:15:37 +0100

    🧐 Testing 2 python modules for analyzing dataset 🤔

--- lego_cli/data_analysis.py
+++ lego_cli/data_analysis.py
@@ -2,11 +2,18 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-
+from ydata_profiling import ProfileReport
+import sweetviz as sv
 
 def analyze_by_theme(csv_path, theme_filter=None, subtheme_filter=None):
     """ Analyzes LEGO data by theme and subtheme. """
     lego_data = pd.read_csv(csv_path)
+
+    # profile = ProfileReport(lego_data, title="Profiling Report")
+    # profile.to_file("report-profiling.html")
+
+    my_report = sv.analyze(lego_data)
+    my_report.show_html("report-sweetviz.html")
 
     # Grouping by Year, Theme, and Subtheme
     theme_counts_by_year = lego_data.groupby(
